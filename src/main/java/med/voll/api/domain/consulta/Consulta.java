38f1bea4a5514d71c854477eff0daf3cc021d154
Wellington Gabriel Monteiro Batista <wellingtongmb@gmail.com>
2024-06-26 10:01:08 -0300

    add tests for MedicoRepository

--- src/main/java/med/voll/api/domain/consulta/Consulta.java
+++ src/main/java/med/voll/api/domain/consulta/Consulta.java
@@ -36,6 +36,8 @@ public class Consulta {
     @Enumerated(EnumType.STRING)
     private MotivoCancelamento motivoCancelamento;
 
+
+
     public void cancelar(MotivoCancelamento motivo) {
         this.motivoCancelamento = motivo;
     }
