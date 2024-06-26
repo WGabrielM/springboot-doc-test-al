b3f1f9b47fb627c9d1856a8c6f38447d8e4f3a5e
Wellington Gabriel Monteiro Batista <wellingtongmb@gmail.com>
2024-06-25 22:42:52 -0300

    add springdoc for documentation

--- src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
+++ src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -32,6 +32,7 @@ public class SecurityConfigurations {
                 // Configurar autorização das requisições
                 .authorizeHttpRequests(req -> {
                     req.requestMatchers("/login").permitAll(); // Permitir acesso irrestrito ao endpoint /login
+                    req.requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll();
                     req.anyRequest().authenticated(); // Requer autenticação para qualquer outra requisição
                 })
 
